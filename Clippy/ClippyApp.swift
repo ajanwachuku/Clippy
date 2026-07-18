07a858ae5fc099e282a64df88d0c6211084e3a50
ajanwachuku <peterajanwachuku@gmail.com>
2026-07-18 11:52:09 +0100

    Initial Commit

---
+++ Clippy/ClippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClippyApp.swift
+//  Clippy
+//
+//  Created by Peter AjaNwachuku on 18/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
